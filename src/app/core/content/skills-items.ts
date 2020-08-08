6b59ef5a23f84b6c495577c71d0d15b0832d547e
sobradovic <s.obradovic@msgnetconomy.net>
2020-08-08 17:27:43 +0200

    Skills content update and new favicon

--- src/app/core/content/skills-items.ts
+++ src/app/core/content/skills-items.ts
@@ -5,65 +5,65 @@ export class SkillsItems {
   public static get CV_SKILLS_ITEM_1(): CvItem {
     return {
       id: 1,
-      headline: 'Network design',
-      description: 'Designing networks based on Cisco devices',
-      imageURI: 'assets/img/icons/NetworkDesignIcon.png'
+      headline: 'Machine Learning',
+      description: 'Machine learning experience working with TensorFlow',
+      imageURI: 'assets/img/skills/machineLearning.png'
     };
   }
   public static get CV_SKILLS_ITEM_2(): CvItem {
     return {
       id: 2,
-      headline: 'Java programming',
-      description: 'Creating server and computer applications',
-      imageURI: 'assets/img/icons/JavaIcon.png'
+      headline: 'DevOps',
+      description: 'Developed CI/CD pipelines for code deployment using Jenkins',
+      imageURI: 'assets/img/skills/devops.png'
     };
   }
   public static get CV_SKILLS_ITEM_3(): CvItem {
     return {
       id: 3,
-      headline: 'Encryption algorithms',
-      description: 'Implementation of AES, DES and others',
-      imageURI: 'assets/img/icons/EncryptionIcon.png'
+      headline: 'Backend programming',
+      description: 'Programming in Java based backend systems like Spring',
+      imageURI: 'assets/img/skills/backend.png'
     };
   }
   public static get CV_SKILLS_ITEM_4(): CvItem {
     return {
       id: 4,
-      headline: 'Database design and optimization',
-      description: 'SQL, MySQL, SQLite, Oracle and others',
-      imageURI: 'assets/img/icons/DatabaseIcon.png'
+      headline: 'Frontend programming',
+      description: 'Frontend development experience in Angular, React, JSP/JSTL',
+      imageURI: 'assets/img/skills/frontend.png'
     };
   }
   public static get CV_SKILLS_ITEM_5(): CvItem {
     return {
       id: 5,
-      headline: 'Linux systems',
-      description: 'Usage of many different distributions',
-      imageURI: 'assets/img/icons/LinuxIcon.png'
+      headline: 'SAP Commerce Cloud',
+      description: 'Creating e-commerce websites in SAP Commerce Cloud',
+      imageURI: 'assets/img/skills/SAPCC.png'
     };
   }
   public static get CV_SKILLS_ITEM_6(): CvItem {
     return {
       id: 6,
-      headline: 'SAP Commerce Cloud',
-      description: 'Creating e-commerce websites',
-      imageURI: 'assets/img/icons/HybrisIcon.png'
+      headline: 'Network programming',
+      description: 'Designing and setting up networks based on Cisco devices',
+      imageURI: 'assets/img/skills/network.png'
     };
   }
   public static get CV_SKILLS_ITEM_7(): CvItem {
     return {
       id: 7,
-      headline: 'SAP Commerce Cloud',
-      description: 'Creating e-commerce websites',
-      imageURI: 'assets/img/icons/HybrisIcon.png'
+      headline: 'Encryption algorithms',
+      description: 'Implementation of encryption algorithms such as AES and DES',
+      imageURI: 'assets/img/skills/encryption.png'
     };
   }
   public static get CV_SKILLS_ITEM_8(): CvItem {
     return {
       id: 8,
-      headline: 'SAP Commerce Cloud',
-      description: 'Creating e-commerce websites',
-      imageURI: 'assets/img/icons/HybrisIcon.png'
+      headline: 'Git',
+      description: 'Experience using software version control systems such as Git and BitBucket',
+      imageURI: 'assets/img/skills/git.png'
     };
   }
 
